d180317b77ad0beb87cf40103be9897312cb4ec4
DESKTOP-CN5LM35\Tom <tomjanecka1667@seznam.cz>
2023-03-13 18:11:20 +0100

    Doplnění SIMD

--- Practice/array-benchmark/main.cpp
+++ Practice/array-benchmark/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <intrin.h>
+#include <stdbool.h>
 
 #include <tracy/Tracy.hpp>
 #include <benchmark/benchmark.h>
@@ -8,7 +9,7 @@
 #define ROWS 5000
 #define COLS 5000
 
-#define N (1 << 8)
+#define N (1 << 28)
 
 int **arr;
 
@@ -26,18 +27,21 @@ void cache_associativity_limit(int step);
 void prefetching();
 void hot_cold();
 void aos_soa();
+void struct_size();
+double array_sum(double *array);
+void array_add(double *A, double *B, double* results);
 
 double vector_add_simd()
 {
     ZoneScopedS(5);
 
     // Vector
-    double* array = (double*)_aligned_malloc(sizeof(double) * N, 16);
+    double *array = (double *)_aligned_malloc(sizeof(double) * N, 16);
 
     // SSE specific step
     int step = 128 / (sizeof(double) * 8);
     int sumSize = step;
-    double* sum = (double*)malloc(sizeof(double) * sumSize);
+    double *sum = (double *)malloc(sizeof(double) * sumSize);
 
     for (int i = 0; i < N; ++i)
     {
@@ -50,7 +54,7 @@ double vector_add_simd()
     }
 
     __m128d sumVector = _mm_loadu_pd(sum);
-    __m128d* arrayVector = (__m128d*)array;
+    __m128d *arrayVector = (__m128d *)array;
     int vecIterations = N / step;
 
     for (int i = 0; i < vecIterations; i++)
@@ -78,7 +82,7 @@ double vector_add()
     ZoneScopedS(5);
 
     // Scalar
-    double* array = (double*)malloc(sizeof(double) * N);
+    double *array = (double *)malloc(sizeof(double) * N);
     double sum = 0.0;
 
     // Array initialization
@@ -96,15 +100,90 @@ double vector_add()
     return sum;
 }
 
+int8_t *add_vectors()
+{
+    ZoneScopedS(5);
+
+    int8_t *A = (int8_t *)_aligned_malloc(sizeof(int8_t) * N, 16);
+    int8_t *B = (int8_t *)_aligned_malloc(sizeof(int8_t) * N, 16);
+    int8_t *C = (int8_t *)_aligned_malloc(sizeof(int8_t) * N, 16);
+    int step = 128 / (sizeof(int8_t) * 8);
+
+    for (int i = 0; i < N; ++i)
+    {
+        A[i] = 1;
+        B[i] = 1;
+    }
+
+    __m128i *AVector = (__m128i *)A;
+    __m128i *BVector = (__m128i *)B;
+    __m128i *CVector = (__m128i *)C;
+
+    for (int i = 0; i < step; i++)
+    {
+        CVector[i] = _mm_add_epi8(AVector[i], BVector[i]);
+    }
+
+    return C;
+}
+
+int8_t *add_scalars()
+{
+    ZoneScopedS(5);
+
+    int8_t *A = (int8_t *)malloc(sizeof(int8_t) * N);
+    int8_t *B = (int8_t *)malloc(sizeof(int8_t) * N);
+    int8_t *C = (int8_t *)malloc(sizeof(int8_t) * N);
+
+    for (int i = 0; i < N; ++i)
+    {
+        A[i] = 1;
+        B[i] = 1;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        C[i] = A[i] + B[i];
+    }
+
+    return C;
+}
+
 int main(void)
 {
     ZoneScopedS(5);
 
-    double res = vector_add();
-    printf("%f\n", res);
+    /*int8_t* resScalar = add_scalars();
+    printf("%d\n", resScalar[0]);
+    free(resScalar);
 
-    res = vector_add_simd();
-    printf("%f\n", res);
+    int8_t* resVector = add_vectors();
+    printf("%d\n", resVector[0]);*/
+    // free(resVector);
+
+    // double res = vector_add();
+    // printf("%f\n", res);
+
+    // res = vector_add_simd();
+    // printf("%f\n", res);
+
+    // struct_size();
+
+    double *A = (double *)malloc(sizeof(double) * N);
+    double *B = (double *)malloc(sizeof(double) * N);
+    double *results = (double *)malloc(sizeof(double) * N);
+
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = 1.0;
+        B[i] = 1.0;
+    }
+
+    double res = array_sum(A);
+    printf("%f", res);
+
+    array_add(A, B, results);
+    printf("%f", results[0]);
 
     // setup();
 
@@ -486,6 +565,55 @@ void aos_soa()
     for (int i = 0; i < n; i++)
     {
         data.results[i] = data.a[i] + data.b[i] * data.c[i];
+    }
+}
+
+void struct_size()
+{
+
+    struct Data
+    {
+        bool a;
+        int32_t b;
+        bool c;
+        int64_t d;
+        bool e;
+    };
+
+    sizeof(Data);
+
+    struct DataOrdered
+    {
+        int64_t a;
+        int32_t b;
+        bool c, d, e;
+    };
+
+    sizeof(DataOrdered);
+
+    printf("Data: %d, Smaller: %d\n", sizeof(Data), sizeof(DataOrdered));
+}
+
+double array_sum(double *array) {
+
+    ZoneScopedS(5);
+
+    double sum = 0.0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += array[i];
+    }
+
+    return sum;
+}
+
+void array_add(double *A, double *B, double* results) {
+
+    ZoneScopedS(5);
+    
+    for (int i = 0; i < N; i++)
+    {
+        results[i] = A[i] + B[i];
     }
 }
 
